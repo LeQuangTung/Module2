6e6ba9f95d2094509faaeeff10dd847e96b03b2d
LeQuangTung <lequangtung1410@gmail.com>
2022-10-31 14:18:54 +0700

    bai 19

---
+++ string_regex/src/PhoneNumber.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PhoneNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập số điện thoại: ");
+        String str = scanner.nextLine();
+        String regex = "^\\d{2}-0\\d{9}";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(str);
+        boolean match = matcher.matches();
+        System.out.println(match);
+    }
+}
